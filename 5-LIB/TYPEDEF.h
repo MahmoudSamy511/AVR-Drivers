c17e30559abe23b42da3add472a4886a04bf7eb4
mahmoud samy <mahmoudsamyzx11@gmail.com>
2023-08-03 20:06:23 +0300

    Modify S32 Macro

--- 5-LIB/TYPEDEF.h
+++ 5-LIB/TYPEDEF.h
@@ -7,7 +7,7 @@ typedef unsigned long int   u32;
 
 typedef signed char         s8;
 typedef signed short int    s16;
-typedef unsigned long int   s32;
+typedef signed long int     s32;
 
 typedef float               f32;
 typedef double              f64;
